f93246211b60e27a10a2bb985adf5c2c31e4485e
Shubham Singh <shubham.suresh.singh@gmail.com>
2019-11-10 18:19:37 -0500

    Adds Repository annotation.

--- src/main/java/com/edu/school/courses/Repository/group/EventRepository.java
+++ src/main/java/com/edu/school/courses/Repository/group/EventRepository.java
@@ -2,6 +2,8 @@ package com.edu.school.courses.Repository.group;
 
 import com.edu.school.courses.model.group.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
--- src/main/java/com/edu/school/courses/Repository/group/GroupRepository.java
+++ src/main/java/com/edu/school/courses/Repository/group/GroupRepository.java
@@ -2,6 +2,8 @@ package com.edu.school.courses.Repository.group;
 
 import com.edu.school.courses.model.group.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
 }
